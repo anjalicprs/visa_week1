1a69bcbe814ec927a2c6a06fbef2c83835a0a75f
Banu Prakash <banuprakashc@yahoo.co.in>
2019-07-09 16:35:27 +0530

    commited till day2

---
+++ oop/src/com/visa/prj/client/Dummy.java
@@ -0,0 +1,12 @@
+package com.visa.prj.client;
+
+ 
+public class Dummy {
+	public static void main(String[] args) {
+		int x  = 10;
+		Integer iX = x ; // auto boxing
+		
+		int y = iX ; // unboxing
+		
+	}
+}
